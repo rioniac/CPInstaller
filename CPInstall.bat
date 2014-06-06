76d811616d4228bcd7380a00d02b8b90f5c83d5e
Sebastian Courtney <rioniac@users.noreply.github.com>
2014-06-05 23:29:47 -0400

    Update CPInstall.bat

--- CPInstall.bat
+++ CPInstall.bat
@@ -1,17 +1,15 @@
 @echo off
 
 REM Set variables, not sure how to pull in version number yet
-set NUM=1234
+SET NUM=1234
 REM set VER=1.0.11
 
 REM Add 7zip to path for use from command line
-set NEW=C:\Program Files\7-Zip\
-set PATH=%PATH%;%NEW%
+SET NEW=C:\Program Files\7-Zip\
+SET PATH=%PATH%;%NEW%
 
 REM extract main zip file
 7z x CRASHPlayer-master.zip
-
-pause
 
 REM move down into directory where widget.xml is
 cd CRASHPlayer-master\CRASHPlayer.widget\Contents\
@@ -22,20 +20,23 @@ SET PowerShellScriptPath=%ThisScriptsDirectory%widgetFix.ps1
 REM call PS script to edit widget.xml
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' -NUM %NUM% -myPath %CD%";
 
-pause
+SET /p VERSION=<ver.txt
 
-set /p VERSION=<%CD%\ver.txt
 DEL "ver.txt"
 REM move back up two directories
 cd ..
 cd ..
 
+@ECHO %VERSION%
+pause
+
 REM Create ZIP file
 7z a com.crashplayer%NUM%.yctv.crashplayer-%VERSION%.zip CRASHPlayer.widget
 
-pause
 
 REM Rename .zip file as a .widget file
 ren com.crashplayer%NUM%.yctv.crashplayer-%VERSION%.zip com.crashplayer%NUM%.yctv.crashplayer-%VERSION%.widget
 
-pause
+REM The below lines are for debugging
+cd ..
+@echo %VERSION% >> ver.txt
