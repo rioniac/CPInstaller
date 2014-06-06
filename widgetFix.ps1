43fcfdeef5dea3b3bf3dd6e0315b7cc59d97f267
Sebastian Courtney <rioniac@users.noreply.github.com>
2014-06-05 23:28:59 -0400

    Update widgetFix.ps1
    Fixed weird txt output issue.

--- widgetFix.ps1
+++ widgetFix.ps1
@@ -6,5 +6,5 @@ cd $myPath
 [xml]$myXML = Get-Content $myPath\widget.xml
 $VER = $myXML.metadata.version
 $myXML.metadata.identifier = "com.crashplayer$NUM.yctv.crashplayer"
-$VER | Out-File $myPath\ver.txt
+Add-Content -Path "$myPath\ver.txt" -Value "$VER"
 $myXML.Save("$myPath\widget.xml")
